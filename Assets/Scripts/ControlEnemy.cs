e5edd1e1cc0ca53a364b916863c4be08e73fba9f
johanPaC115 <johanandrescp@ufps.edu.co>
2020-07-29 20:30:37 -0500

    Script Enemy Lizard

---
+++ Assets/Scripts/ControlEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlEnemy : MonoBehaviour
+{
+    public float vel = -1f;
+    Rigidbody2D rgb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // 
+    void FixedUpdate()
+    {
+        Vector2 v = new Vector2(vel, 0);
+        rgb.velocity = v;
+
+    }
+}
