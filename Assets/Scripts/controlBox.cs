a39150a495167e5733126acfef0f0f259c5151a8
johanPaC115 <johanandrescp@ufps.edu.co>
2021-01-09 19:30:29 -0500

    contro,de colision caja

---
+++ Assets/Scripts/controlBox.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlBox : MonoBehaviour
+{
+    [SerializeField]
+    private int numGolpes = 8;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GolpePlayer()
+    {
+        bool resp = false;
+        --numGolpes;
+        if (numGolpes <= 2) {
+            animator.SetBool("golpeOne", true);
+            if (numGolpes<=0)
+            {
+                animator.SetBool("golpeOne", false);
+                animator.SetBool("golpeTwo", true);
+                resp = true;
+                Destroy(gameObject, .9f);
+            }
+        }
+        Debug.Log("Respuesta: "+resp);
+        return resp;       
+    }
+}
