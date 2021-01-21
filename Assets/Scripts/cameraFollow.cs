8a1503f6bf84653cc6e389dfedd8ed758b970990
johanPaC115 <johanandrescp@ufps.edu.co>
2021-01-20 19:02:03 -0500

    Seguimiento de la Camara

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float xMax, yMax, xMin, yMin;
+
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("knight").transform;
+    }
+
+     
+    void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax), transform.position.z);
+    }
+}
